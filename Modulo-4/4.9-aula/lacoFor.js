ce75c5718bf5971f521f8aa3df99eea753d9805e
Dani Lima <31131965+dnijlima@users.noreply.github.com>
2021-08-28 14:12:51 -0300

    Add files via upload

---
+++ Modulo-4/4.9-aula/lacoFor.js
@@ -0,0 +1,18 @@
+/**
+ * arquivo: condicional.js
+ * descrição: explicação de como podemos usar For
+ * author: Dani Lima 
+ * data:02/08/2021
+ */
+
+//Laço for
+/*for(let i=1; i <= 10; i++) {
+    console.log('O número é...: ', i);
+
+}*/
+ // [] => indica que estamos trabalhando como arays
+//otro ex: laço for
+let frutas = ["Maça", "Banana", "Manga", "Laranja", "Abacaxi",];
+for(let i=0; i < frutas.length; i++){
+    console.log('A fruta é ...: ', frutas[i])
+}
